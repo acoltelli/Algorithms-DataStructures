f14f7fb722a27afc9078051639693085c6458d38
Alle <allecoltelli@gmail.com>
2018-12-17 21:28:24 -0800

    add tests

--- Sorting.py
+++ Sorting.py
@@ -1,8 +1,11 @@
+import unittest
+
 def bubbleSort(arr):
 	for i in range(0, len(arr)):
 		for j in range(0,len(arr)-1):
 			if arr[j] > arr[j+1]:
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+        return arr
 
 def insertionSort(arr):
     for i in range(1,len(arr)):
@@ -13,9 +16,23 @@ def insertionSort(arr):
         arr[i] = current
     return arr
 
+ 
 
 
+###tests###
 
+class Test(unittest.TestCase):   
+    def testBubbleSort(self):
+      self.assertEqual(bubbleSort([3, 1, 2, 4, 6, 5, 3, 1]), [1, 1, 2, 3, 3, 4, 5, 6])
+      self.assertEqual(bubbleSort([3, 1, 0]), [0, 1, 3])
+
+    def testInsertionSort(self):
+      self.assertEqual(insertionSort([3, 1, 2, 4, 6, 5, 3, 1]), [1, 1, 2, 3, 3, 4, 5, 6])
+      self.assertEqual(insertionSort([0, 1, 2, 4, 6, 5, 3]),[0, 1, 2, 3, 4, 5, 6])
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 
 
