a5d1e0c256cfdc9f18326e9101f3cf5457296c78
Alle <allecoltelli@gmail.com>
2018-12-26 16:22:45 -0800

    tests

--- Sorting.py
+++ Sorting.py
@@ -46,28 +46,43 @@ def mergeSort(arr):
       return merged   
 
 
+
 ###tests###
 
 class Test(unittest.TestCase):   
     def testBubbleSort(self):
       self.assertEqual(bubbleSort([3, 1, 2, 4, 6, 5, 3, 1]), [1, 1, 2, 3, 3, 4, 5, 6])
       self.assertEqual(bubbleSort([3, 1, 0]), [0, 1, 3])
+      self.assertEqual(bubbleSort([1,2,3]),[1,2,3])
+      self.assertEqual(bubbleSort([0]),[0])
 
     def testInsertionSort(self):
       self.assertEqual(insertionSort([3, 1, 2, 4, 6, 5, 3, 1]), [1, 1, 2, 3, 3, 4, 5, 6])
       self.assertEqual(insertionSort([0, 1, 2, 4, 6, 5, 3]),[0, 1, 2, 3, 4, 5, 6])
+      self.assertEqual(insertionSort([1,2,3]),[1,2,3])
+      self.assertEqual(insertionSort([7]),[7])
 
     def testQuickSort(self):
       self.assertEqual(quickSort([10,9,8,7,6,5,4,3,2,1]), [1,2,3,4,5,6,7,8,9,10])
       self.assertEqual(quickSort([7,7,7,7,7,7,7]), [7,7,7,7,7,7,7])
       self.assertEqual(quickSort([5,4,3,2,1, 10, 0, 5,5, 0]), [0, 0, 1, 2, 3, 4, 5, 5, 5, 10])
       self.assertEqual(quickSort([5,5,1,4,3,2,5,1, 10,0,0]), [0, 0, 1, 1, 2, 3, 4, 5, 5, 5, 10])
+      self.assertEqual(quickSort([1,2,3,4,5]), [1,2,3,4,5])
+      self.assertEqual(quickSort([7]), [7])
 
     def testQuickSortMid(self):
       self.assertEqual(quickSortMid([10,9,8,7,6,5,4,3,2,1]), [1,2,3,4,5,6,7,8,9,10])
       self.assertEqual(quickSort([7,7,7,7,7,7,7]), [7,7,7,7,7,7,7])
       self.assertEqual(quickSortMid([5,4,3,2,1, 10, 0, 5,5, 0]), [0, 0, 1, 2, 3, 4, 5, 5, 5, 10])
       self.assertEqual(quickSortMid([5,5,1,4,3,2,5,1, 10,0,0]), [0, 0, 1, 1, 2, 3, 4, 5, 5, 5, 10])
+      self.assertEqual(quickSortMid([1,2,3,4,5]), [1,2,3,4,5])
+      self.assertEqual(quickSortMid([7]), [7])
+
+    def testMergeSort(self):
+      self.assertEqual(mergeSort([3,2,1, 4,6,5,7,8,9,10,11,0,200000000,12,13,14,15,0,16,1,1,10]),[0,0,1,1,1,2,3,4,5,6,7,8,9,10,10,11,12,13,14,15,16,200000000])
+      self.assertEqual(mergeSort([5,0,7,2,4,8]), [0,2,4,5,7,8])
+      self.assertEqual(mergeSort([1,2,3,4,5]), [1,2,3,4,5])
+      self.assertEqual(mergeSort([7]),[7])
 
 
 if __name__ == "__main__":
