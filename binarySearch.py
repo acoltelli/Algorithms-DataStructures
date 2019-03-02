561d19a97fc832748608eb69010c7ec1aef787aa
Alle <allecoltelli@gmail.com>
2019-03-02 14:03:11 -0800

    tests

--- binarySearch.py
+++ binarySearch.py
@@ -1,3 +1,5 @@
+import unittest
+
 #resursive 
 def binarySearch(arr, n):
     if len(arr) == 0:
@@ -14,12 +16,21 @@ def binarySearch(arr, n):
       	return binarySearch(arr[:mid], n)
 
         
+class Test(unittest.TestCase): 
+	def testBSRecursive(self):
+		arr = [0, 1, 2 ,3 , 4, 5, 6,7, 8, 9]
+		self.assertEqual(binarySearch(arr, 3), True)
+		self.assertEqual(binarySearch(arr, -1), False)
+		self.assertEqual(binarySearch(arr, 0), True)
+		arr1 = [-13, 0 , 15, 1009, -7]
+		self.assertEqual(binarySearch(arr1, -13), True)
+		self.assertEqual(binarySearch(arr1, 0), True)
+		self.assertEqual(binarySearch(arr1, 27), False)
+		self.assertEqual(binarySearch(arr1, -27), False)
 
 
-testlist = [0, 1, -2, 8, 13, 17, 19, 32, 42,]
-print(binarySearch(testlist, 3))
-print(binarySearch(testlist, 13))
 
 
-
+if __name__ == "__main__":
+    unittest.main()
 
