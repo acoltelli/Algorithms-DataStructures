9e5632a020b6476e9fedd0b0bb09ccd17181185f
Alle <allecoltelli@gmail.com>
2019-03-01 16:00:28 -0800

    powerset iterative

--- PowerSet.py
+++ PowerSet.py
@@ -3,5 +3,10 @@ def powerset(arr):
     if not arr: return [[]]
     return powerset(arr[1:]) + [[arr[0]] + i for i in powerset(arr[1:])]
 
+#iterative
 
-
+def ps(arr):
+    sol = [[]]
+    for i in arr:
+        sol.extend([j + [i] for j in sol])
+    return sol
