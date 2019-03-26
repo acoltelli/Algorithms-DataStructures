58d4b3e50804b79beb0c945425b60e4d235bdf92
acoltelli <acoltelli@protonmail.com>
2019-03-26 16:57:34 -0700

    memoization nth fib

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,6 +1,6 @@
 
-
-def nthFibonacci (n):
+#recursive
+def nthFibonacci(n):
 	if n == 0 or n == 1:
 		return n 
 	else:
@@ -8,4 +8,13 @@ def nthFibonacci (n):
 
 
 
+#memoization
+memo = {0: 0, 1: 1}
+def nthFibonacci_(n):
+    if n not in memo:
+        nthval = nthFibonacci_(n-1) + nthFibonacci_(n-2)
+        memo[n] = nthval
+    return memo[n]
 
+print nthFibonacci(10)
+print nthFibonacci_(10)
