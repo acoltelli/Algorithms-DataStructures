88d0251b7cc8eb8023b9a07dbbbc12de02276092
acoltelli <acoltelli@protonmail.com>
2019-03-26 18:52:29 -0700

    testing time

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,3 +1,5 @@
+import timeit
+
 
 #recursive
 def nthFibonacci(n):
@@ -13,6 +15,22 @@ def nthFibonacci_(n, memo ={0:0,1:1}):
         nthval = nthFibonacci_(n-1) + nthFibonacci_(n-2) #compute recursively if val not already saved in memo
         memo[n] = nthval #save this computed number in memo
     return memo[n]
+
+
+
+start = timeit.default_timer()
+nthFibonacci(25)
+a= timeit.default_timer() - start
+print "recursive " + str(a)
+
+start = timeit.default_timer()
+nthFibonacci_(25)
+b= timeit.default_timer() - start
+print round(b, 100)
+print "memoization "+ str(b)  
+print "delta " + str(a-b) 
+
+
 
 
 
