863c71cc681050a44c70e831ec4ee7e55941a368
acoltelli <acoltelli@protonmail.com>
2019-03-26 17:41:40 -0700

    memoization

--- Fibonacci.py
+++ Fibonacci.py
@@ -7,14 +7,20 @@ def nthFibonacci(n):
 		return nthFibonacci(n-1) + nthFibonacci(n-2)
 
 
-
 #memoization
-memo = {0: 0, 1: 1}
-def nthFibonacci_(n):
+def nthFibonacci_(n, memo ={0:0,1:1}):
     if n not in memo:
-        nthval = nthFibonacci_(n-1) + nthFibonacci_(n-2)
-        memo[n] = nthval
+        nthval = nthFibonacci_(n-1) + nthFibonacci_(n-2) #compute recursively if val not already saved in memo
+        memo[n] = nthval #save this computed number in memo
     return memo[n]
 
-print nthFibonacci(10)
-print nthFibonacci_(10)
+
+
+
+
+
+
+
+
+
+
