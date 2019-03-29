e693a6c6abf0f3b188cd9c9516823807a0adc66f
acoltelli <acoltelli@protonmail.com>
2019-03-28 21:03:58 -0700

    memoization

--- LongestCommonSubsequence.py
+++ LongestCommonSubsequence.py
@@ -1,7 +1,5 @@
 
-
 #subsequences are not required to be consecutive elements 
-
 #recursive
 def lcs(X, Y, m, n): 
     if m == 0 or n == 0: return 0 
@@ -12,20 +10,16 @@ def lcs(X, Y, m, n):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# memoization 
+def lcs_(X , Y): 
+    memo = [[None]*(len(Y)+1) for i in range(len(X)+1)] #create matrix memo, initially filled with None 
+  
+    for i in range(len(X)+1): 
+        for j in range(len(Y)+1): 
+            if i == 0 or j == 0 : 
+                memo[i][j] = 0
+            elif X[i-1] == Y[j-1]: 
+                memo[i][j] = memo[i-1][j-1]+1
+            else: 
+                memo[i][j] = max(memo[i-1][j] , memo[i][j-1]) 
+    return memo[len(X)][len(Y)] # return bottom right most element of matrix 
