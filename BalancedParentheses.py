33c6fb61cb095f1e0fe80fab9fde8486ae412776
acoltelli <acoltelli@protonmail.com>
2019-04-02 13:37:51 -0700

    search for matching pairs

--- BalancedParentheses.py
+++ BalancedParentheses.py
@@ -1,21 +1,30 @@
-from Stack import Stack
-
-
+from Stack import *
 
 
 def parentheses(str):
-	stack = Stack(len(str))
-	 
-	for i in str:
-		if i == '(':
-			stack.push(i)
-		if i == ')':
-			stack.pop()
+    stack = Stack(len(str))
+
+    for i in str:    
+        if i in "([{":
+            stack.push(i)
+        else: # )]}
+            top = stack.pop()
+            if not pair(top, i): #does the last item added to stack match(type of parenthese) current item
+                return False #if does not match, means that parentheses in str are out of order and not matching
+    if stack.isEmpty():
+        return True
+    return False
+
+def pair(open, close):
+    return "([{".index(open) == ")]}".index(close)  # check if type of parenthese matches 
 
 
-	if stack
-	print stack
+print parentheses('{{([][])}()}')
+print parentheses('[{()]')
+print parentheses('{(([])[])[]]}')
+print parentheses('{(([])[])[]}[]')
 
 
-parentheses('((()))')
-parentheses('(()')
+
+
+
