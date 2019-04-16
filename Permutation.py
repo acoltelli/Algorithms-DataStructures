d2fadd06432af6698283f63c0c9fb6cac594a826
acoltelli <acoltelli@protonmail.com>
2019-04-15 19:22:10 -0700

    comments

--- Permutation.py
+++ Permutation.py
@@ -6,10 +6,11 @@ def permutation(arr, n):
         for i in range(n-1):
             for p in permutation(arr, n-1): 
             	yield p
-            if n % 2 == 0: # odd length arr
-            	arr[i], arr[n-1] = arr[n-1], arr[i] #swap two elements 
+            if n % 2 == 0: # even len arr
+            	#zero indexed, called on permutation(n-1), so n is n-1 etc. 
+            	arr[i], arr[n-1] = arr[n-1], arr[i] #swap elements
             else: 
-            	arr[0], arr[n-1] = arr[n-1], arr[0]
+            	arr[0], arr[n-1] = arr[n-1], arr[0] 
 
         for p in permutation(arr, n-1): 
         	yield p
