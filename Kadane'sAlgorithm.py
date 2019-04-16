5896f3d5981d96ec7d58a1f475ee039c51703027
acoltelli <acoltelli@protonmail.com>
2019-04-16 16:51:21 -0700

    Kadane's algorithm for max subarray

---
+++ Kadane'sAlgorithm.py
@@ -0,0 +1,9 @@
+def maxSubArray(nums): 
+	maxSubUpToHere = maxTotal = nums[0] #pick first element as initial max sub
+	for i in nums[1:]:
+		#maximum subarray is either the current element
+		#or the current element combined with the previous maximum subarray
+		maxSubUpToHere = max(i, maxSubUpToHere + i)
+		maxTotal = max(maxTotal, maxSubUpToHere) 
+	return maxTotal
+
