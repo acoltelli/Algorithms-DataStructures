556b9415c0705ae07eaaa86102951f19c0de02f7
acoltelli <acoltelli@protonmail.com>
2019-04-25 16:15:13 -0700

    dp sol, fewest num coins

--- CoinChange.py
+++ CoinChange.py
@@ -1,14 +1,36 @@
 import unittest
 
+#recursive sol 
 #find the number of ways to sum values in arr to get n 
 def coinChange(arr, len, n): 
     if n == 0: 
         return 1  #n=0, include 0 values- 1 sol
     if n < 0 or (len == 0 and n >= 1): 
         return 0 
-    # sol is sum of solutions not including last term in arr, plus solutions including last term
+    # sol is sum of solutions not including last term in arr + solutions including last term
     return coinChange(arr, len - 1, n) + coinChange(arr, len, n - arr[len-1]); 
   
+
+#dynamic programming sol 
+#compute the fewest number of coins, items in arr, that you need to sum to n
+def coinChangeDP(arr, n):
+    dp = [0] + [n+1] * n
+    # print dp
+    for i in range(1, n+1):
+        for j in arr:
+            if i >= j:
+                dp[i] = min(dp[i], dp[i-j] + 1)
+                # print dp
+    if dp[n] == n+1:
+        return None #if n cannot be made up of any combination in arr
+    return dp[n]
+
+
+
+
+
+
+
 
 
 
@@ -22,6 +44,14 @@ class Test(unittest.TestCase):
 		self.assertEqual(coinChange([1, 2, 3], len([1, 2, 3]), 0), 1)
 		self.assertEqual(coinChange([1, 2, 3], len([1, 2, 3]), -5), 0)
 		self.assertEqual(coinChange([], len([]), 5), 0)
+	def testCoingChangeDP(self):
+		self.assertEqual(coinChangeDP([1, 2, 5], 11), 3)
+		self.assertEqual(coinChangeDP([1, 5, 10], 12), 3)
+		self.assertEqual(coinChangeDP([2, 5, 10], 12), 2)
+
+		self.assertEqual(coinChangeDP([2],3), None)
+		self.assertEqual(coinChangeDP([1],0), 0)
+
 
 
 
