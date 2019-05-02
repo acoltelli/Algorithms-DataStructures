485002f052b3ccedd6f54101f1a8f66938393975
acoltelli <acoltelli@protonmail.com>
2019-05-01 18:38:57 -0700

    add recusive sol, inefficient

--- CoinChange.py
+++ CoinChange.py
@@ -11,16 +11,31 @@ def coinChange(arr, len, n):
     return coinChange(arr, len - 1, n) + coinChange(arr, len, n - arr[len-1]); 
   
 
+#recusive sol without using memoization
+#compute the fewest number of coins you need to sum to n
+def coinChange_(arr, n):
+   arr = [i for i in arr if i <= n]	#eliminate coins of value greater than n 
+   minCoins = n
+   if n in arr:
+     return 1 #base case where there is a coin in arr equal to n
+   else:
+      for i in arr:
+         numCoins = 1 + coinChange_(arr, n - i)
+         if numCoins < minCoins:
+            minCoins = numCoins
+
+   return minCoins
+
+
+
 #dynamic programming sol 
-#compute the fewest number of coins, items in arr, that you need to sum to n
+#compute the fewest number of coins you need to sum to n
 def coinChangeDP(arr, n):
     dp = [0] + [n+1] * n # first index set to 0 because there is 0 ways to sum to 0 from arr
-    # print dp
     for i in range(1, n+1):
         for j in arr:
             if i >= j:
                 dp[i] = min(dp[i], dp[i-j] + 1)
-                # print dp
     if dp[n] == n+1:
         return None #if n cannot be made up of any combination in arr
     return dp[n]
