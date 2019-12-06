04bd6aa4a48d99887c814236cb7b6757cfef4f02
acoltelli <acoltelli@protonmail.com>
2019-12-05 17:45:43 -0800

    test

--- maxHeight.py
+++ maxHeight.py
@@ -1,3 +1,7 @@
+import unittest
+
+
+
  # A binary tree node
 class Node:
     # node constructor
@@ -19,3 +23,25 @@ def maxDepth(node):
             return lDepth+1
         else:
             return rDepth+1
+
+
+
+
+
+
+class Test(unittest.TestCase):
+    def testMaxDepth(self):
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+
+        self.assertEqual(maxDepth(root), 3)
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
