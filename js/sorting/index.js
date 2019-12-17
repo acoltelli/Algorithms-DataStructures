0251b710f256c58a06ce2acae7d56e6367f1925a
acoltelli <acoltelli@protonmail.com>
2019-12-17 15:35:50 -0800

    merge sort

--- js/sorting/index.js
+++ js/sorting/index.js
@@ -3,7 +3,7 @@
 
 function bubbleSort(arr) {
   for (let i=0; i < arr.length; i++) {
-    for (let j= 0; j < (arr.length-i-1); j++){
+    for (let j= 0; j < (arr.length-i); j++){
       if (arr[j] > arr[j+1]){
         const less = arr[j+1];
         arr[j+1] = arr[j];
@@ -34,11 +34,28 @@ function selectionSort(arr) {
 
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center, arr.length);
 
+  return merge(mergeSort(left), mergeSort(right) );
 }
 
 function merge(left, right) {
-
+    const result = [];
+    while (left.length && right.length) {
+      if (left[0] < right[0]){
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
+      }
+    }
+    return [...result, ...left, ...right];
 }
+
+
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
